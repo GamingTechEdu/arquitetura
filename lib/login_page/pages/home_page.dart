fbbf517d79c045f89d4e637154b7052acab204c3
Eduardo <gamingtechedu@gmail.com>
2023-10-10 10:32:15 -0300

    v0.0.3 Eduardo Henrique

--- lib/login_page/pages/home_page.dart
+++ lib/login_page/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:package_teste/login_page/controller/home_controller.dart';
+import 'package:package_teste/login_page/models/post_model.dart';
+import 'package:package_teste/login_page/repositories/home_repository.dart';
 import 'package:package_teste/login_page/repositories/home_repository_imp.dart';
 import 'package:package_teste/login_page/repositories/home_repository_mock.dart';
-
-import '../models/post_model.dart';
+import 'package:package_teste/login_page/service/prefs_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,19 +15,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryImp());
+  final _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
-    super.initState();
     _controller.fetch();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Posts'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              PrefsService.logout();
+              Navigator.of(context).pushNamedAndRemoveUntil('/login', (_) => true);
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
       ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
@@ -33,12 +44,18 @@ class _HomePageState extends State<HomePage> {
           return ListView.separated(
             itemCount: list.length,
             itemBuilder: (_, idx) => ListTile(
-              leading: Text(list[idx].id.toString(),style: const TextStyle(fontSize: 20),),
-              trailing: const Icon(Icons.arrow_forward),
               title: Text(list[idx].title),
-              onTap: () => print(list[idx].title),
+              trailing: Icon(Icons.arrow_forward),
+              leading: Text(
+                list[idx].id.toString(),
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushNamed('/details', arguments: list[idx]);
+              },
             ),
-            separatorBuilder: (_,__) => const Divider(),
+            separatorBuilder: (_, __) => Divider(),
           );
         },
       ),
